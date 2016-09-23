7f15f3085b9e5a2cd902ce55dfa0e0d82f9ff1ef
Helge Eichhorn <git@helgeeichhorn.de>
2016-09-23 21:57:11 +0200

    Add build script.

---
+++ deps/build.jl
@@ -0,0 +1,13 @@
+using BinDeps
+
+const BASE_URL = "http://downloads.sourceforge.net/project/gmat/GMAT"
+const RELEASE = "R2015a"
+
+@BinDeps.setup
+
+gmat = library_dependency("libCInterface")
+provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-macosx-x64-$RELEASE.tar.gz"), gmat, os=:Darwin, unpacked_dir="GMAT/$RELEASE/bin")
+provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-ubuntu-x64-$RELEASE.tar.gz"), gmat, os=:Linux, unpacked_dir="GMAT/$RELEASE/bin")
+provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-win-i586-$RELEASE.zip"), gmat, os=:Windows, unpacked_dir="GMAT/$RELEASE/bin")
+
+@BinDeps.install Dict(:libCInterface => :libCInterface)
