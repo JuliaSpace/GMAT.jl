beefd81bfd1b282ace5daeff91ba3332eb5921a0
Helge Eichhorn <git@helgeeichhorn.de>
2016-09-23 21:57:28 +0200

    Test drive C API.

--- src/GMAT.jl
+++ src/GMAT.jl
@@ -1,5 +1,63 @@
 module GMAT
 
-# package code goes here
+using Compat
+
+const RELEASE = "R2015a"
+const GMAT_DIR = joinpath(@__DIR__, "..", "deps", "GMAT", RELEASE, "bin")
+
+const DEPS = joinpath(@__DIR__, "..", "deps", "deps.jl")
+if isfile(DEPS)
+    include(DEPS)
+else
+    error("GMAT was not found. Please run 'Pkg.build(\"GMAT\").")
+end
+
+type GmatError <: Exception end
+Base.showerror(io::IO, err::GmatError) = print(io, "GMAT Error: ", last_message());
+
+function start(dir)
+    cd(dir) do
+        code = ccall((:StartGmat, libCInterface), Cint, ())
+        code != 0  && throw(GmatError())
+        return code
+    end
+end
+start() = start(GMAT_DIR)
+
+function load(dir, file)
+    cd(dir) do
+        code = ccall((:LoadScript, libCInterface), Cint, (Cstring,), file)
+        code != 0  && throw(GmatError())
+        return code
+    end
+end
+load(path) = load(splitdir(path)...)
+
+function run_summary()
+    summary = ccall((:GetRunSummary, libCInterface), Cstring, ())
+    unsafe_string(summary)
+end
+
+function state_description()
+    desc = ccall((:GetStateDescription, libCInterface), Cstring, ())
+    desc != C_NULL ? unsafe_string(desc) : ""
+end
+
+function state_size()
+    ccall((:GetStateSize, libCInterface), Cint, ())
+end
+
+function run()
+    cd(GMAT_DIR) do
+        code = ccall((:RunScript, libCInterface), Cint, ())
+        code != 0  && throw(GmatError())
+    end
+    print_message()
+end
+
+function last_message()
+    unsafe_string(ccall((:getLastMessage, libCInterface), Cstring, ()))
+end
+print_message() = info(last_message())
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,18 @@
 using GMAT
+using Compat
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+@testset "Init" begin
+    @test GMAT.start() == 0
+    @test_throws GMAT.GmatError GMAT.start(@__DIR__)
+end
+
+@testset "Load & Run" begin
+    file = "/Users/helge/Downloads/GMAT/R2015a/samples/Ex_HohmannTransfer.script"
+    GMAT.start()
+    @test GMAT.load(file) == 0
+println(GMAT.run())
+println(GMAT.state_size())
+println(GMAT.state_description())
+end
