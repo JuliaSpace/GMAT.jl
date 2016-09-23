db86c91b53e63a19c8d6222df5d40754ee21ebe9
Helge Eichhorn <git@helgeeichhorn.de>
2016-09-23 22:15:30 +0200

    Fix Windows path.

--- deps/build.jl
+++ deps/build.jl
@@ -8,6 +8,6 @@ const RELEASE = "R2015a"
 gmat = library_dependency("libCInterface")
 provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-macosx-x64-$RELEASE.tar.gz"), gmat, os=:Darwin, unpacked_dir="GMAT/$RELEASE/bin")
 provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-ubuntu-x64-$RELEASE.tar.gz"), gmat, os=:Linux, unpacked_dir="GMAT/$RELEASE/bin")
-provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-win-i586-$RELEASE.zip"), gmat, os=:Windows, unpacked_dir="GMAT/$RELEASE/bin")
+provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-win-i586-$RELEASE.zip"), gmat, os=:Windows, unpacked_dir="GMAT/bin")
 
 @BinDeps.install Dict(:libCInterface => :libCInterface)
