9048385d938e2192408910a85e0da65676c45fc9
Helge Eichhorn <git@helgeeichhorn.de>
2016-09-24 18:12:54 +0200

    BinDeps y u do dis?

--- deps/build.jl
+++ deps/build.jl
@@ -6,12 +6,12 @@ const RELEASE = "R2015a"
 @BinDeps.setup
 
 gmat = library_dependency("libCInterface", aliases=["libCInterface.so.$RELEASE"])
-@static if is_apple()
-    provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-macosx-x64-$RELEASE.tar.gz"), gmat, unpacked_dir="GMAT/$RELEASE/bin")
-elseif is_linux()
+#= @static if is_apple() =#
+#=     provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-macosx-x64-$RELEASE.tar.gz"), gmat, unpacked_dir="GMAT/$RELEASE/bin") =#
+#= elseif is_linux() =#
     provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-ubuntu-x64-$RELEASE.tar.gz"), gmat, unpacked_dir="GMAT/$RELEASE/bin")
-else
-    provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-win-i586-$RELEASE.zip"), gmat, unpacked_dir="GMAT/bin")
-end
+#= else =#
+#=     provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-win-i586-$RELEASE.zip"), gmat, unpacked_dir="GMAT/bin") =#
+#= end =#
 
 @BinDeps.install Dict(:libCInterface => :libCInterface)
