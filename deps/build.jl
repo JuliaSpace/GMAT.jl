108eb1cd4e4eed301c895f269a782b2645ae8dc8
Helge Eichhorn <git@helgeeichhorn.de>
2016-09-24 18:20:32 +0200

    BinDeps :-(

--- deps/build.jl
+++ deps/build.jl
@@ -5,11 +5,11 @@ const RELEASE = "R2015a"
 
 @BinDeps.setup
 
-gmat = library_dependency("libCInterface", aliases=["libCInterface.so.$RELEASE"])
+libCInterface = library_dependency("libCInterface", aliases=["libCInterface.so.$RELEASE"])
 #= @static if is_apple() =#
 #=     provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-macosx-x64-$RELEASE.tar.gz"), gmat, unpacked_dir="GMAT/$RELEASE/bin") =#
 #= elseif is_linux() =#
-    provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-ubuntu-x64-$RELEASE.tar.gz"), gmat, unpacked_dir="GMAT/$RELEASE/bin")
+    provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-ubuntu-x64-$RELEASE.tar.gz"), libCInterface, unpacked_dir="GMAT/$RELEASE/bin")
 #= else =#
 #=     provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-win-i586-$RELEASE.zip"), gmat, unpacked_dir="GMAT/bin") =#
 #= end =#
