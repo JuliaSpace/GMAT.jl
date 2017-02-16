ad7bbcfb6b1a4b74f1c239ff32398c996c682149
Helge Eichhorn <git@helgeeichhorn.de>
2017-02-16 13:15:24 +0100

    Cleanup build.jl.

--- deps/build.jl
+++ deps/build.jl
@@ -9,8 +9,8 @@ const RELEASE = "R2016a"
     push!(BinDeps.defaults, Binaries)
 end
 gmat = library_dependency("libCInterface")
-provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-macosx-x64-$RELEASE.tar.gz"), gmat, os=:Darwin, unpacked_dir="GMAT/$RELEASE/bin")
-provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-ubuntu-x64-$RELEASE.tar.gz"), gmat, os=:Linux, unpacked_dir="GMAT/$RELEASE/bin")
-provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-win-i586-$RELEASE.zip"), gmat, os=:Windows, unpacked_dir="GMAT/bin")
+provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-macosx-x64-$RELEASE.tar.gz"), gmat, os=:Darwin, unpacked_dir=joinpath("GMAT", RELEASE, "bin"))
+provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-ubuntu-x64-$RELEASE.tar.gz"), gmat, os=:Linux, unpacked_dir=joinpath("GMAT", RELEASE, "bin"))
+provides(Binaries, URI("$BASE_URL/GMAT-$RELEASE/gmat-win-i586-$RELEASE.zip"), gmat, os=:Windows, unpacked_dir=joinpath("GMAT", "bin"))
 
 @BinDeps.install Dict(:libCInterface => :libCInterface)
